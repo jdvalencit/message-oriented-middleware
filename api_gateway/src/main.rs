6106f6fb3439cda093eb13f7523f477654dff15c
ITAKEB <tomascf123@gmail.com>
2023-04-10 01:17:30 -0500

    Add basic-auth and refactor some routes

--- api_gateway/src/main.rs
+++ api_gateway/src/main.rs
@@ -1,4 +1,6 @@
 use dotenv::dotenv;
+use rocket_basicauth::BasicAuth;
+
 #[macro_use]
 extern crate rocket;
 use api_gateway::grpc_client::grpc_client::{
@@ -10,36 +12,44 @@ fn index() -> &'static str {
     "Welcome to my mom"
 }
 
-#[get("/create_queue/<name>")]
-async fn create_queue(name: String) -> String {
+#[post("/register-user")]
+async fn register_user(auth: BasicAuth) -> String {
+    println!("{:?}", auth.username);
+    println!("{:?}", auth.password);
+    "Fake".to_string()
+}
+
+
+#[post("/create-queue/<name>")]
+async fn create_queue(name: String, auth: BasicAuth) -> String {
     grpc_create(name).await
 }
 
-#[get("/read_queue/<id_queue>")]
+#[get("/read-queue/<id_queue>")]
 async fn read_queue(id_queue: String) -> String {
     grpc_read(id_queue.clone()).await
 }
 
-#[get("/delete_queue/<id_queue>")]
-async fn delete_queue(id_queue: String) -> String {
+#[delete("/delete-queue/<id_queue>")]
+async fn delete_queue(id_queue: String, auth: BasicAuth) -> String {
     grpc_delete(id_queue.clone()).await
 }
 
-#[get("/put/<id_queue>/<content>")]
+#[put("/put/<id_queue>/<content>")]
 async fn put(id_queue: String, content: String) -> String {
     grpc_put(id_queue.clone(), content.clone()).await
 }
 
-#[get("/get/<id_queue>")]
-async fn get(id_queue: String) -> String {
-    grpc_get(id_queue.clone()).await
+#[get("/get-queues")]
+async fn get() -> String {
+    grpc_get("fake".to_string()).await
 }
 
 #[launch]
 fn rocket() -> _ {
     dotenv().ok();
     rocket::build()
-        .mount("/", routes![index])
+        .mount("/", routes![index, get, register_user])
         .mount("/crud", routes![create_queue, read_queue, delete_queue])
-        .mount("/queue", routes![put, get])
+        .mount("/queue", routes![put])
 }
